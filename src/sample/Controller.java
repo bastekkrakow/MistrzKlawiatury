3f1fded268c4e896dbfffaf5b0372e95755856bc
Sebastian Plonka <bastekkrakow@gmail.com>
2018-04-17 16:54:35 +0200

    update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,68 +1,60 @@
 package sample;
 
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyEvent;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Controller {
 
-    StringBuilder input1 = new StringBuilder();
-    char[] tablica = new char[100];
-    int counter;
-    int blad;
-    char a;
-    char b;
-    String napis;
-    String napis2 = "Tutaj wklej tekst lub załaduj z menu:";
-    ArrayList lista = new ArrayList();
+
+    private String napis;
+    private String napis2 = "Tutaj wklej tekst lub załaduj z menu:\n" +
+            "public class PolishWords {\n" +
+            "    public static void main(String[] args) {\n" +
+            "        File file = new File(\"jakis_path\");\n" +
+            "        try {\n" +
+            "            InputStreamReader inputStreamReader =\n" +
+            "                    new InputStreamReader(new FileInputStream(file), \"UTF-8\")\n" +
+            "        } catch (UnsupportedEncodingException e) {\n" +
+            "            e.printStackTrace();\n" +
+            "        } catch (FileNotFoundException e) {\n" +
+            "            e.printStackTrace();\n" +
+            "        }   ";
+
+    private char[] tablica = new char[napis2.length()];
 
     @FXML
     TextArea pisz;
 
     @FXML
     Label labelka;
+
     @FXML
     Label errors;
 
     @FXML
     TextArea oknoTekstu;
 
-
     @FXML
     public void pisze() {
         pisz.addEventFilter(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
+
                 napis = pisz.getText();
                 tablica = napis.toCharArray();
-                counter = tablica.length - 1;
-                System.out.println(tablica);
                 labelka.setText("Pozostało znaków: " + (napis2.length() - tablica.length));
-                typer();
-
+                char[] tab1 = napis.toCharArray();
+                char[] tab2 = napis2.toCharArray();
+                int blad = 0;
+                for (int i = 0; i < tab1.length; i++) {
+                    if (tab1[i] != tab2[i]) blad++;
+                }
+                errors.setText("Liczba błędów: " + blad);
             }
         });
     }
-
-    @FXML
-    public void typer() {
-        char a = tablica[counter];
-        char b = napis2.charAt(counter);
-        System.out.println(tablica);
-        System.out.println(napis2);
-        System.out.println(a + " " + b);
-        if (a != b) {
-            blad = blad + 1;
-            errors.setText("Liczba błędów: " + blad);
-        }
-
-    }
-
-
 }
